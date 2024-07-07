c87707e4f84753308c4a38e1c0ea1d122037980a
obsidian <obsidiancrystal@tutanota.com>
2024-07-07 20:55:17 +0530

    modified:   voting.sol

--- voting.sol
+++ voting.sol
@@ -1,22 +1,26 @@
+// From 'learn solidity by example', the official documentation
+// https://docs.soliditylang.org/en/v0.5.2/solidity-by-example.html
 pragma solidity 0.8.26;
 
 contract Ballot {
-				struct Voter {
-								uint weight;
-								bool voted;
-								address delegate;
-								uint vote;
-				}
+	struct Voter {
+		uint weight;
+		bool voted;
+		address delegate;
+		uint vote;
+	}
 
-				struct Proposal {
-								bytes32 name; // Name of the proposal
-								uint voteCount; // Total number of votes received
-				}
+	struct Proposal {
+		bytes32 name; // Name of the proposal
+		uint voteCount; // Total number of votes received
+	}
 
-				address public 
+	address public chairPerson; 
 
+	// Mapping addresses to each voter struct,
+	mapping(address => Voter) public voters;
 
-
+	Proposal[] public proposals;
 
 
 
